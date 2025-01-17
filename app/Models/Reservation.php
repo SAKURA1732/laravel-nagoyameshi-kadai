87c54dc18c2e09418a9b535b8b53dfdb9e4d1d41
SAKURA1732 <ymdakk8490@gmail.com>
2025-01-17 23:54:37 +0900

    Reservation

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\HTTP\Middleware\Subscribed;
 
 class Reservation extends Model
 {
