f5629c38df3ca90d43561656043416d3efd9b455
SAKURA1732 <ymdakk8490@gmail.com>
2025-01-18 00:10:41 +0900

    Reservation

--- app/Http/Middleware/Subscribed.php
+++ app/Http/Middleware/Subscribed.php
@@ -3,13 +3,11 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class Subscribed
 {
-    // 有料プランに登録済みであることを確認するためのミドルウェア
     /**
      * Handle an incoming request.
      *
@@ -17,11 +15,10 @@ class Subscribed
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if ($request->user() && ! $request->user()?->subscribed('premium_plan')){
-            // ユーザーを有料プラン登録ページへリダイレクトし、有料プランに登録するか尋ねる
+        if (! $request->user()?->subscribed('premium_plan')) {
+            // ユーザーを支払いページへリダイレクトし、サブスクリプションを購入するか尋ねる
             return redirect('subscription/create');
         }
-
         return $next($request);
     }
 }
