b004690e4603161d746e130505ea1eee8accdca7
SAKURA1732 <ymdakk8490@gmail.com>
2025-01-18 20:19:33 +0900

    Review

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,6 +13,46 @@ use Laravel\Cashier\Billable;
 class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable, Billable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'kana',
+        'email',
+        'password',
+        'postal_code',
+        'address',
+        'phone_number',
+        'birthday',
+        'occupation',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+
+
+
     public function reviews(){
         return $this->hasMany(Review::class);
     }
@@ -24,4 +64,5 @@ class User extends Authenticatable implements MustVerifyEmail
     public function favorite_restaurants() {
         return $this->belongsToMany(Restaurant::class, 'restaurant_user', 'user_id', 'restaurant_id');
     }
+
 }
